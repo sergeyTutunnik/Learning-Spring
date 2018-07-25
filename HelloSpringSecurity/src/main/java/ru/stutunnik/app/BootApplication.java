0407f62bece38015b7c28ebea6be1409c59bcee0
mac <sergey.tutunnik@gmail.com>
2018-07-25 10:15:54 +0300

    Basic Spring Security project. In memory users with roles introduction. Logout endpoint.

---
+++ HelloSpringSecurity/src/main/java/ru/stutunnik/app/BootApplication.java
@@ -0,0 +1,66 @@
+package ru.stutunnik.app;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SpringBootApplication
+@EnableWebSecurity
+@Controller
+public class BootApplication extends WebSecurityConfigurerAdapter {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("q1").password("{noop}1").roles("USER").and()
+                .withUser("admin").password("{noop}1").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+
+        http
+
+                .authorizeRequests()
+                .antMatchers("/greet/hi/**").hasRole("ADMIN")  // requests login and permits hi access to 'ADMIN' roles
+                //.antMatchers("/greet/hi/**").authenticated() // requests login
+                .antMatchers("/greet/**").permitAll() // allows unlogined users to access all 'greet' uris
+                .anyRequest().authenticated()
+                //.antMatchers("/**").hasRole("ADMIN")  //HAS TO BE ABOVE .anyRequest().authenticated()!!!
+                .and()
+                .formLogin().and()
+                .httpBasic();
+    }
+
+    //Logout endpoint
+    // {http://websystique.com/spring-security/spring-security-4-logout-example/}
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
+        //You can redirect wherever you want, but generally it's a good practice to show login screen again.
+    }
+
+}
